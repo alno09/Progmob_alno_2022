e5abc0344271e950cf2614f2e12ad93291f63de9
Alno Sabetha <alnosabetha09@gmail.com>
2022-09-25 23:43:10 +0700

    Tugas 3

---
+++ app/src/main/java/com/example/a72190334_progmob_2022/TableActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a72190334_progmob_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table)
+    }
+}
---
+++ app/src/main/java/com/example/a72190334_progmob_2022/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.a72190334_progmob_2022.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
