96acf35dbe5f8089c4b9a26231953b96ee3d7aa2
Alno Sabetha <alnosabetha09@gmail.com>
2022-10-16 22:54:02 +0700

    tugas 7

---
+++ app/src/main/java/com/example/a72190334_progmob_2022/ServiceActivity.kt
@@ -0,0 +1,21 @@
+package com.example.a72190334_progmob_2022
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class ServiceActivity : AppCompatActivity() {
+    lateinit var btnGetAPI : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service)
+
+        btnGetAPI.setOnClickListener( View.OnClickListener { view ->
+            var intent = Intent(this@ServiceActivity,GetAPIActivity::class.java)
+            startActivity(intent)
+        } )
+    }
+}
