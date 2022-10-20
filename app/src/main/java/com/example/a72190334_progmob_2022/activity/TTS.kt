281f3246966e1b57d477cc5df4dd5e29cfeef31c
Alno Sabetha <alnosabetha09@gmail.com>
2022-10-20 09:18:51 +0700

    TTS

---
+++ app/src/main/java/com/example/a72190334_progmob_2022/activity/TTS.kt
@@ -0,0 +1,12 @@
+package com.example.a72190334_progmob_2022.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a72190334_progmob_2022.R
+
+class TTS : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tts2)
+    }
+}
---
+++ app/src/main/java/com/example/a72190334_progmob_2022/adapter/ShowAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.a72190334_progmob_2022.adapter
+
+class ShowAdapter {
+}
